570ee3a25f6af95248c3c1db0e7a481b7353c345
RoyJia <v-rjia@expedia.com>
2018-03-17 22:58:04 +0800

    upgrade HttpHelper

--- src/main/java/com/jjuina/utils/HttpHelper.java
+++ src/main/java/com/jjuina/utils/HttpHelper.java
@@ -3,19 +3,10 @@ package com.jjuina.utils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class HttpHelper {
     /**
@@ -27,23 +18,27 @@ public class HttpHelper {
     public static JSONObject sendGetByHttpURLConnection(String requestUrl) {
         String res = "";
         JSONObject object = null;
-        StringBuffer buffer = new StringBuffer();
+        StringBuffer content = new StringBuffer();
         try {
             URL url = new URL(requestUrl);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            if (200 == urlConnection.getResponseCode()) {
-                InputStream inputStream = urlConnection.getInputStream();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            int status = connection.getResponseCode();
+            if (200 == status) {
+                InputStream inputStream = connection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-                String str = null;
-                while ((str = bufferedReader.readLine()) != null) {
-                    buffer.append(str);
+                String inputLine;
+                while ((inputLine = bufferedReader.readLine()) != null) {
+                    content.append(inputLine);
                 }
+
                 bufferedReader.close();
                 inputStreamReader.close();
                 inputStream.close();
-                res = buffer.toString();
+                connection.disconnect();
+
+                res = content.toString();
                 object = new JSONObject(res);
             }
         } catch (IOException e) {
@@ -52,33 +47,40 @@ public class HttpHelper {
         return object;
     }
 
-    public static JSONObject setPostByHttpURLConnect(String requestUrl, String params) {
+    public static JSONObject setPostByHttpURLConnect(String requestUrl, Map<String, String> params) {
         URL url = null;
         try {
             url = new URL(requestUrl);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("POST");
-            // httpURLConnection.setConnectTimeout(10000);
-            // httpURLConnection.setReadTimeout(2000);
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setDoInput(true);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            // connection.setConnectTimeout(10000);
+            // connection.setReadTimeout(2000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
 
             // send data
-            PrintWriter printWriter = new PrintWriter(httpURLConnection.getOutputStream());
-            printWriter.write(params);
-            printWriter.flush();
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            out.writeBytes(ParameterStringBuilder.getParameterString(params));
+            out.flush();
+            out.close();
 
-            //            get data
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+            // get data
+            int status = connection.getResponseCode();
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            int len;
-            byte[] arr = new byte[1024];
-            while ((len = bufferedInputStream.read(arr)) != -1) {
-                byteArrayOutputStream.write(arr, 0, len);
-                byteArrayOutputStream.flush();
+            if(200 == status) {
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(connection.getInputStream());
+                int len;
+                byte[] arr = new byte[1024];
+                while ((len = bufferedInputStream.read(arr)) != -1) {
+                    byteArrayOutputStream.write(arr, 0, len);
+                    byteArrayOutputStream.flush();
+                }
+                byteArrayOutputStream.close();
             }
-            byteArrayOutputStream.close();
-            return new JSONObject(byteArrayOutputStream.toString());
+            return !"".equals(byteArrayOutputStream.toString())
+                    ? new JSONObject(byteArrayOutputStream.toString())
+                    : new JSONObject();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -87,11 +89,11 @@ public class HttpHelper {
 
     public static void main(String args [] ) {
         JSONObject res = null;
-        //        res = sendGetByHttpURLConnection("https://sem-keyword-entity-recognition-service.us-east-1.bigdata.expedia.com/entity/parse?locale=en_IE&siteId=4400&keyword=Florence");
-        ////        res = setPostByHttpURLConnect("http://ip.taobao.com/service/getIpInfo.php?ip=63.223.108.42","123");
-        //        System.out.println(res);
-        //        System.out.println(res.get("code"));
-        //        System.out.println(res.get("data"));
+//        res = sendGetByHttpURLConnection("https://sem-keyword-entity-recognition-service.us-east-1.bigdata.expedia.com/entity/parse?locale=en_IE&siteId=4400&keyword=Florence");
+        res = setPostByHttpURLConnect("http://ip.taobao.com/service/getIpInfo.php?ip=63.223.108.42", new HashMap<String, String>());
+        System.out.println(res);
+        System.out.println(res.get("code"));
+        System.out.println(res.get("data"));
 
 //        List<String> result = new ArrayList<String>();
 //        String[] dess = {"Florence", "Faro", "Budapest"};
